da7012f0abbeedc6c9067c400c85ba8333e4697e
Daiki Horiyama <d19921207@gmail.com>
2020-01-04 23:57:49 +0900

    setup serverless

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.json', '.tsx'],
+    alias: {
+      "@app": path.resolve(__dirname, 'src')
+    }
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader' },
+    ],
+  },
+};
