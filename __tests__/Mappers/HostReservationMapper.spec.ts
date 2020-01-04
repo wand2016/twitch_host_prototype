2fe1591f4d00821a6ebe8f0efeda22d36220ef97
Daiki Horiyama <d19921207@gmail.com>
2020-01-05 03:01:20 +0900

    DataMapperクラス

---
+++ __tests__/Mappers/HostReservationMapper.spec.ts
@@ -0,0 +1,20 @@
+import HostReservationMapper from "@app/Mappers/HostReservationMapper";
+import event from "./sample/event";
+import HostReservation from "@app/Domain/HostReservation";
+import DatetimeRange from "@app/Domain/DatetimeRange";
+
+test("createFromGoogleCalendarEvent/所定のJSONから所定のHostReservationを得る", () => {
+  const hostReservationMapper = new HostReservationMapper();
+  const hostReservation = hostReservationMapper.createFromGoogleCalendarEvent(event);
+
+  expect(hostReservation).toEqual(
+    new HostReservation(
+      "an_awesome_streamer",
+      new DatetimeRange(
+        new Date("2020-01-04T17:00:00+09:00"),
+        new Date("2020-01-04T18:00:00+09:00"),
+      ),
+      new Date("2020-01-04T12:13:08.218Z"),
+    ),
+  );
+});
---
+++ src/Mappers/HostReservationMapper.ts
@@ -0,0 +1,18 @@
+"use strict";
+
+import { calendar_v3 } from "googleapis";
+import HostReservation from "@app/Domain/HostReservation";
+import DatetimeRange from "@app/Domain/DatetimeRange";
+
+export default class HostReserationMapper {
+  public createFromGoogleCalendarEvent(event: calendar_v3.Schema$Event): HostReservation {
+    return new HostReservation(
+      event.summary.split("\n")[0],
+      new DatetimeRange(
+        new Date(event.start.dateTime),
+        new Date(event.end.dateTime),
+      ),
+      new Date(event.updated),
+    );
+  }
+}
